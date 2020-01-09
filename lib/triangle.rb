eee728cf6fff02da6c19acc990bdb7b7e5c881e0
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 03:56:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+        puts error.message
+    end
+    
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle."
+    end
+  end
+  
 end
+
+equilateral  3 equal sides 
+isosceles    2 equal sides
+scalene      0 equal sides
